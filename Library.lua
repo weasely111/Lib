aabfa775f4ea4ef1db67d176af7a22e008b08752
weasely111 <weasely@soyboy.observer>
2025-02-23 10:00:06 +0000

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -23,13 +23,6 @@ local Library = {
 
 	HudRegistry = {};
 
-	FontColor = Color3.fromRGB(255, 255, 255);
-	MainColor = Color3.fromRGB(28, 28, 28);
-	BackgroundColor = Color3.fromRGB(20, 20, 20);
-	AccentColor = Color3.fromRGB(220, 220, 220);
-	OutlineColor = Color3.fromRGB(40, 40, 40);
-	RiskColor = Color3.fromRGB(255, 50, 50),
-
 	Black = Color3.new(0, 0, 0);
 	Font = Enum.Font.Code,
 
